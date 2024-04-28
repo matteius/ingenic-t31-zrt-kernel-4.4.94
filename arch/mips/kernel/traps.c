5758c71499124704c6795a161f5cb6fa80855ff9
Matt Davis <matteius@gmail.com>
2024-04-27 23:08:25 -0400

    Compat change for 4.6

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -427,13 +427,9 @@ static const struct exception_table_entry *search_dbe_tables(unsigned long addr)
 {
     const struct exception_table_entry *e;
 
-    if (__stop___dbe_table > __start___dbe_table) {
-        e = search_extable(__start___dbe_table, __stop___dbe_table - __start___dbe_table, addr);
-        if (!e)
-            e = search_module_dbetables(addr);
-    } else {
-        e = NULL;
-    }
+    e = search_extable(__start___dbe_table, __stop___dbe_table, addr);
+    if (!e)
+        e = search_module_dbetables(addr);
 
     return e;
 }
