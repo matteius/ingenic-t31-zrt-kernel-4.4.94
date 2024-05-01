6f1635c5da1f19719e772c96e48c5db5435143ed
Matt Davis <matteius@gmail.com>
2024-05-01 18:10:14 -0400

    Ingenic wdt driver needs work (missing include)

--- drivers/watchdog/ingenic_wdt.c
+++ drivers/watchdog/ingenic_wdt.c
@@ -28,6 +28,7 @@
 #include <linux/of.h>
 #include <linux/delay.h>
 #include <linux/sched.h>
+#include <linux/uaccess.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/delay.h>
