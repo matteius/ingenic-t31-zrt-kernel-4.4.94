1eaa251590f0ee53520ff77098a0a53dd6cf023a
Matt Davis <matteius@gmail.com>
2024-05-05 14:25:48 -0400

    Build ipu driver

--- drivers/clocksource/clksrc-probe.c
+++ drivers/clocksource/clksrc-probe.c
@@ -26,30 +26,27 @@ static const struct of_device_id __clksrc_of_table_sentinel
 
 void __init clocksource_probe(void)
 {
-	struct device_node *np;
-	const struct of_device_id *match;
-	of_init_fn_1_ret init_func_ret;
-	unsigned clocksources = 0;
-	int ret;
+    struct device_node *np;
+    const struct of_device_id *match;
+    of_init_fn_1_ret init_func_ret;
+    unsigned clocksources = 0;
+    int ret;
 
-	for_each_matching_node_and_match(np, __clksrc_of_table, &match) {
-		if (!of_device_is_available(np))
-			continue;
+    for_each_matching_node_and_match(np, __clksrc_of_table, &match) {
+        if (!of_device_is_available(np))
+            continue;
 
-		init_func_ret = match->data;
+        init_func_ret = match->data;
+        ret = init_func_ret(np);
+        if (ret) {
+            pr_err("Failed to initialize '%s': %d",
+                   of_node_full_name(np), ret);
+            continue;
+        }
 
-		ret = init_func_ret(np);
-		if (ret) {
-			pr_err("Failed to initialize '%s': %d",
-			       of_node_full_name(np), ret);
-			continue;
-		}
+        clocksources++;
+    }
 
-		clocksources++;
-	}
-
-	clocksources += acpi_probe_device_table(clksrc);
-
-	if (!clocksources)
-		pr_crit("%s: no matching clocksources found\n", __func__);
+    if (!clocksources)
+        pr_crit("%s: no matching clocksources found\n", __func__);
 }
