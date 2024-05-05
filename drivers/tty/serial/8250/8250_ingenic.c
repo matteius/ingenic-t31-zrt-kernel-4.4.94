02c802cb55e15a16ad652cd45977c3559ba4e0bb
Matt Davis <matteius@gmail.com>
2024-05-04 23:40:08 -0400

    tweak on uart driver for buad rate

--- drivers/tty/serial/8250/8250_ingenic.c
+++ drivers/tty/serial/8250/8250_ingenic.c
@@ -297,8 +297,9 @@ static int ingenic_uart_probe(struct platform_device *pdev)
     if (data->clk_module)
         clk_prepare_enable(data->clk_module);
 
-    // Ensure baud rate divisor is set for 115200 baud rate.
-    uart.port.custom_divisor = uartclk / 115200;
+    /* Set baud rate divisor for 115200 baud */
+    writel(uart.port.custom_divisor & 0xff, uart.port.membase + (UART_DLL << uart.port.regshift));
+    writel((uart.port.custom_divisor >> 8) & 0xff, uart.port.membase + (UART_DLM << uart.port.regshift));
 
     data->line = serial8250_register_8250_port(&uart);
     if (data->line < 0) {
