71116b969d6f9af5005e462fc6e01727a6c60e9c
Matt Davis <matteius@gmail.com>
2024-05-07 10:58:43 -0400

    iterate on shim

---
+++ include/misc/isp_mem_map.h
@@ -0,0 +1,10 @@
+//
+// Created by matteius on 5/7/24.
+//
+
+#ifndef INGENIC_T31_ZRT_KERNEL_4_4_94_MATTEIUS_ISP_MEM_MAP_H
+#define INGENIC_T31_ZRT_KERNEL_4_4_94_MATTEIUS_ISP_MEM_MAP_H
+
+void __iomem *get_isp_base(void);
+
+#endif //INGENIC_T31_ZRT_KERNEL_4_4_94_MATTEIUS_ISP_MEM_MAP_H
