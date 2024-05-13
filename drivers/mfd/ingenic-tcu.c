6abc682d04ab531719922c9f650b534584ea29f4
Matt Davis <matteius@gmail.com>
2024-05-13 00:31:46 -0400

    Check pt updated tcu driver

--- drivers/mfd/ingenic-tcu.c
+++ drivers/mfd/ingenic-tcu.c
@@ -3,15 +3,19 @@
  * JZ47xx SoCs TCU clocks driver
  * Copyright (C) 2019 Paul Cercueil <paul@crapouillou.net>
  */
-
+#include <linux/module.h>
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/clockchips.h>
 #include <linux/mfd/ingenic-tcu.h>
 #include <linux/mfd/syscon.h>
-#include <linux/regmap.h>
 #include <linux/slab.h>
 #include <linux/syscore_ops.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_clk.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
 
 #include <dt-bindings/clock/ingenic,tcu.h>
 
@@ -49,7 +53,7 @@ struct ingenic_tcu_clk {
 
 struct ingenic_tcu {
 	const struct ingenic_soc_info *soc_info;
-	struct regmap *map;
+	void __iomem *map;  // Direct memory-mapped I/O address
 	struct clk *clk;
 
 	struct clk_hw_onecell_data *clocks;
@@ -65,32 +69,22 @@ static inline struct ingenic_tcu_clk *to_tcu_clk(struct clk_hw *hw)
 static int ingenic_tcu_enable(struct clk_hw *hw)
 {
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
-	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
-	struct ingenic_tcu *tcu = tcu_clk->tcu;
-
-	regmap_write(tcu->map, TCU_REG_TSCR, BIT(info->gate_bit));
-
+	writel(readl(tcu_clk->tcu->map + TCU_REG_TSCR) | BIT(tcu_clk->info->gate_bit),
+		   tcu_clk->tcu->map + TCU_REG_TSCR);
 	return 0;
 }
 
 static void ingenic_tcu_disable(struct clk_hw *hw)
 {
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
-	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
-	struct ingenic_tcu *tcu = tcu_clk->tcu;
-
-	regmap_write(tcu->map, TCU_REG_TSSR, BIT(info->gate_bit));
+	writel(readl(tcu_clk->tcu->map + TCU_REG_TSSR) | BIT(tcu_clk->info->gate_bit),
+		   tcu_clk->tcu->map + TCU_REG_TSSR);
 }
 
 static int ingenic_tcu_is_enabled(struct clk_hw *hw)
 {
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
-	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
-	unsigned int value;
-
-	regmap_read(tcu_clk->tcu->map, TCU_REG_TSR, &value);
-
-	return !(value & BIT(info->gate_bit));
+	return readl(tcu_clk->tcu->map + TCU_REG_TSR) & BIT(tcu_clk->info->gate_bit);
 }
 
 static bool ingenic_tcu_enable_regs(struct clk_hw *hw)
@@ -100,12 +94,9 @@ static bool ingenic_tcu_enable_regs(struct clk_hw *hw)
 	struct ingenic_tcu *tcu = tcu_clk->tcu;
 	bool enabled = false;
 
-	/*
-	 * According to the programming manual, a timer channel's registers can
-	 * only be accessed when the channel's stop bit is clear.
-	 */
+	// Check if enabled and ensure that register access is allowed.
 	enabled = !!ingenic_tcu_is_enabled(hw);
-	regmap_write(tcu->map, TCU_REG_TSCR, BIT(info->gate_bit));
+	writel(readl(tcu->map + TCU_REG_TSCR) | BIT(info->gate_bit), tcu->map + TCU_REG_TSCR);
 
 	return enabled;
 }
@@ -116,18 +107,16 @@ static void ingenic_tcu_disable_regs(struct clk_hw *hw)
 	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
 	struct ingenic_tcu *tcu = tcu_clk->tcu;
 
-	regmap_write(tcu->map, TCU_REG_TSSR, BIT(info->gate_bit));
+	writel(readl(tcu->map + TCU_REG_TSSR) | BIT(info->gate_bit), tcu->map + TCU_REG_TSSR);
 }
 
 static u8 ingenic_tcu_get_parent(struct clk_hw *hw)
 {
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
 	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
-	unsigned int val = 0;
-	int ret;
+	unsigned int val;
 
-	ret = regmap_read(tcu_clk->tcu->map, info->tcsr_reg, &val);
-	WARN_ONCE(ret < 0, "Unable to read TCSR %d", tcu_clk->idx);
+	val = readl(tcu_clk->tcu->map + info->tcsr_reg);
 
 	return ffs(val & TCU_TCSR_PARENT_CLOCK_MASK) - 1;
 }
@@ -137,13 +126,14 @@ static int ingenic_tcu_set_parent(struct clk_hw *hw, u8 idx)
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
 	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
 	bool was_enabled;
-	int ret;
+	unsigned int val;
 
 	was_enabled = ingenic_tcu_enable_regs(hw);
 
-	ret = regmap_update_bits(tcu_clk->tcu->map, info->tcsr_reg,
-							 TCU_TCSR_PARENT_CLOCK_MASK, BIT(idx));
-	WARN_ONCE(ret < 0, "Unable to update TCSR %d", tcu_clk->idx);
+	val = readl(tcu_clk->tcu->map + info->tcsr_reg);
+	val &= ~TCU_TCSR_PARENT_CLOCK_MASK;
+	val |= BIT(idx);
+	writel(val, tcu_clk->tcu->map + info->tcsr_reg);
 
 	if (!was_enabled)
 		ingenic_tcu_disable_regs(hw);
@@ -157,11 +147,8 @@ static unsigned long ingenic_tcu_recalc_rate(struct clk_hw *hw,
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
 	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
 	unsigned int prescale;
-	int ret;
 
-	ret = regmap_read(tcu_clk->tcu->map, info->tcsr_reg, &prescale);
-	WARN_ONCE(ret < 0, "Unable to read TCSR %d", tcu_clk->idx);
-
+	prescale = readl(tcu_clk->tcu->map + info->tcsr_reg);
 	prescale = (prescale & TCU_TCSR_PRESCALE_MASK) >> TCU_TCSR_PRESCALE_LSB;
 
 	return parent_rate >> (prescale * 2);
@@ -195,18 +182,19 @@ static long ingenic_tcu_round_rate(struct clk_hw *hw, unsigned long req_rate,
 static int ingenic_tcu_set_rate(struct clk_hw *hw, unsigned long req_rate,
 								unsigned long parent_rate)
 {
+	bool was_enabled;
+	unsigned int val;
 	struct ingenic_tcu_clk *tcu_clk = to_tcu_clk(hw);
 	const struct ingenic_tcu_clk_info *info = tcu_clk->info;
+
 	u8 prescale = ingenic_tcu_get_prescale(parent_rate, req_rate);
-	bool was_enabled;
-	int ret;
 
 	was_enabled = ingenic_tcu_enable_regs(hw);
 
-	ret = regmap_update_bits(tcu_clk->tcu->map, info->tcsr_reg,
-							 TCU_TCSR_PRESCALE_MASK,
-							 prescale << TCU_TCSR_PRESCALE_LSB);
-	WARN_ONCE(ret < 0, "Unable to update TCSR %d", tcu_clk->idx);
+	val = readl(tcu_clk->tcu->map + info->tcsr_reg);
+	val &= ~TCU_TCSR_PRESCALE_MASK;
+	val |= prescale << TCU_TCSR_PRESCALE_LSB;
+	writel(val, tcu_clk->tcu->map + info->tcsr_reg);
 
 	if (!was_enabled)
 		ingenic_tcu_disable_regs(hw);
@@ -262,13 +250,15 @@ static const struct ingenic_tcu_clk_info ingenic_tcu_ost_clk_info =
 		DEF_TIMER("ost", 15, TCU_REG_OST_TCSR);
 #undef DEF_TIMER
 
-static int __init ingenic_tcu_register_clock(struct ingenic_tcu *tcu,
+static int ingenic_tcu_register_clock(struct ingenic_tcu *tcu,
 											 unsigned int idx, enum tcu_clk_parent parent,
 											 const struct ingenic_tcu_clk_info *info,
 											 struct clk_hw_onecell_data *clocks)
 {
 	struct ingenic_tcu_clk *tcu_clk;
 	int err;
+	unsigned int val;
+	bool was_enabled;
 
 	tcu_clk = kzalloc(sizeof(*tcu_clk), GFP_KERNEL);
 	if (!tcu_clk)
@@ -279,10 +269,14 @@ static int __init ingenic_tcu_register_clock(struct ingenic_tcu *tcu,
 	tcu_clk->info = info;
 	tcu_clk->tcu = tcu;
 
-	/* Reset channel and clock divider, set default parent */
-	ingenic_tcu_enable_regs(&tcu_clk->hw);
-	regmap_update_bits(tcu->map, info->tcsr_reg, 0xffff, BIT(parent));
-	ingenic_tcu_disable_regs(&tcu_clk->hw);
+	// Ensure the timer is stopped and set its default parent clock
+	was_enabled = ingenic_tcu_enable_regs(&tcu_clk->hw);
+	val = readl(tcu->map + info->tcsr_reg);
+	val &= ~0xffff;  // Assuming mask needs to clear specific bits
+	val |= BIT(parent);
+	writel(val, tcu->map + info->tcsr_reg);
+	if (!was_enabled)
+		ingenic_tcu_disable_regs(&tcu_clk->hw);
 
 	err = clk_hw_register(NULL, &tcu_clk->hw);
 	if (err) {
@@ -320,7 +314,7 @@ static const struct ingenic_soc_info x1000_soc_info = {
 		.allow_missing_tcu_clk = true,
 };
 
-static const struct of_device_id __maybe_unused ingenic_tcu_of_match[] __initconst = {
+static const struct of_device_id ingenic_tcu_of_match[] = {
 		{ .compatible = "ingenic,jz4740-tcu", .data = &jz4740_soc_info, },
 		{ .compatible = "ingenic,jz4725b-tcu", .data = &jz4725b_soc_info, },
 		{ .compatible = "ingenic,jz4760-tcu", .data = &jz4770_soc_info, },
@@ -329,63 +323,67 @@ static const struct of_device_id __maybe_unused ingenic_tcu_of_match[] __initcon
 		{ /* sentinel */ }
 };
 
-static int __init ingenic_tcu_probe(struct device_node *np)
-{
-	const struct of_device_id *id = of_match_node(ingenic_tcu_of_match, np);
+static int ingenic_tcu_probe(struct platform_device *pdev) {
+	const struct of_device_id *id = of_match_node(ingenic_tcu_of_match, pdev->dev.of_node);
 	struct ingenic_tcu *tcu;
-	struct regmap *map;
-	unsigned int i;
+	struct resource *res;
+	void __iomem *base;
 	int ret;
+	int i;
 
-	map = syscon_node_to_regmap(np);
-	if (IS_ERR(map))
-		return PTR_ERR(map);
+	printk("ingenic_tcu_probe\n");
 
-	tcu = kzalloc(sizeof(*tcu), GFP_KERNEL);
-	if (!tcu)
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		dev_err(&pdev->dev, "No memory resource for TCU\n");
+		return -ENOENT;
+	}
+
+	base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(base)) {
+		dev_err(&pdev->dev, "Failed to map TCU registers\n");
+		return PTR_ERR(base);
+	}
+
+	tcu = devm_kzalloc(&pdev->dev, sizeof(*tcu), GFP_KERNEL);
+	if (!tcu) {
+		printk("ingenic_tcu_probe: ENOMEM\n");
 		return -ENOMEM;
+	}
 
-	tcu->map = map;
+	tcu->map = base;
 	tcu->soc_info = id->data;
 
 	if (tcu->soc_info->has_tcu_clk) {
-		tcu->clk = of_clk_get_by_name(np, "tcu");
+		tcu->clk = devm_clk_get(&pdev->dev, "tcu");
 		if (IS_ERR(tcu->clk)) {
 			ret = PTR_ERR(tcu->clk);
-
-			/*
-			 * Old device trees for some SoCs did not include the
-			 * TCU clock because this driver (incorrectly) didn't
-			 * use it. In this case we complain loudly and attempt
-			 * to continue without the clock, which might work if
-			 * booting with workarounds like "clk_ignore_unused".
-			 */
 			if (tcu->soc_info->allow_missing_tcu_clk && ret == -EINVAL) {
-				pr_warn("TCU clock missing from device tree, please update your device tree\n");
+				printk("TCU clock missing from device tree, please update your device tree\n");
 				tcu->clk = NULL;
 			} else {
-				pr_crit("Cannot get TCU clock from device tree\n");
-				goto err_free_tcu;
+				dev_err(&pdev->dev, "Cannot get TCU clock from device tree\n");
+				return ret;
 			}
 		} else {
 			ret = clk_prepare_enable(tcu->clk);
 			if (ret) {
-				pr_crit("Unable to enable TCU clock\n");
-				goto err_put_clk;
+				dev_err(&pdev->dev, "Unable to enable TCU clock\n");
+				return ret;
 			}
 		}
 	}
 
-	tcu->clocks = kzalloc(struct_size(tcu->clocks, hws, TCU_CLK_COUNT),
-						  GFP_KERNEL);
+	tcu->clocks = devm_kzalloc(&pdev->dev, struct_size(tcu->clocks, hws, TCU_CLK_COUNT), GFP_KERNEL);
 	if (!tcu->clocks) {
-		ret = -ENOMEM;
-		goto err_clk_disable;
+		dev_err(&pdev->dev, "Failed to allocate memory for clock data\n");
+		return -ENOMEM;
 	}
 
 	tcu->clocks->num = TCU_CLK_COUNT;
 
 	for (i = 0; i < tcu->soc_info->num_channels; i++) {
+		printk("ingenic_tcu_probe:ingenic_tcu_register_clock\n");
 		ret = ingenic_tcu_register_clock(tcu, i, TCU_PARENT_EXT,
 										 &ingenic_tcu_clk_info[i],
 										 tcu->clocks);
@@ -406,7 +404,7 @@ static int __init ingenic_tcu_probe(struct device_node *np)
 									 &ingenic_tcu_watchdog_clk_info,
 									 tcu->clocks);
 	if (ret) {
-		pr_crit("cannot register watchdog clock\n");
+		printk("cannot register watchdog clock\n");
 		goto err_unregister_timer_clocks;
 	}
 
@@ -416,39 +414,25 @@ static int __init ingenic_tcu_probe(struct device_node *np)
 										 &ingenic_tcu_ost_clk_info,
 										 tcu->clocks);
 		if (ret) {
-			pr_crit("cannot register ost clock\n");
+			printk("cannot register ost clock\n");
 			goto err_unregister_watchdog_clock;
 		}
 	}
 
-	ret = of_clk_add_hw_provider(np, of_clk_hw_onecell_get, tcu->clocks);
-	if (ret) {
-		pr_crit("cannot add OF clock provider\n");
-		goto err_unregister_ost_clock;
-	}
-
+	of_clk_add_hw_provider(pdev->dev.of_node, of_clk_hw_onecell_get, tcu->clocks);
 	ingenic_tcu = tcu;
 
 	return 0;
 
 	err_unregister_ost_clock:
 	if (tcu->soc_info->has_ost)
-		clk_hw_unregister(tcu->clocks->hws[i + 1]);
+		clk_hw_unregister(tcu->clocks->hws[TCU_CLK_OST]);
 	err_unregister_watchdog_clock:
-	clk_hw_unregister(tcu->clocks->hws[i]);
+	clk_hw_unregister(tcu->clocks->hws[TCU_CLK_WDT]);
 	err_unregister_timer_clocks:
-	for (i = 0; i < tcu->clocks->num; i++)
+	for (i = 0; i < tcu->soc_info->num_channels; i++)
 		if (tcu->clocks->hws[i])
 			clk_hw_unregister(tcu->clocks->hws[i]);
-	kfree(tcu->clocks);
-	err_clk_disable:
-	if (tcu->clk)
-		clk_disable_unprepare(tcu->clk);
-	err_put_clk:
-	if (tcu->clk)
-		clk_put(tcu->clk);
-	err_free_tcu:
-	kfree(tcu);
 	return ret;
 }
 
@@ -475,19 +459,27 @@ static struct syscore_ops __maybe_unused tcu_pm_ops = {
 		.resume = tcu_pm_resume,
 };
 
-static void __init ingenic_tcu_init(struct device_node *np)
-{
-	int ret = ingenic_tcu_probe(np);
 
-	if (ret)
-		pr_crit("Failed to initialize TCU clocks: %d\n", ret);
+static struct platform_driver ingenic_tcu_driver = {
+		.driver = {
+				.name = "ingenic-tcu",
+				.of_match_table = ingenic_tcu_of_match,
+		},
+		.probe = ingenic_tcu_probe,
+};
 
-	if (IS_ENABLED(CONFIG_PM_SLEEP))
-		register_syscore_ops(&tcu_pm_ops);
+static int ingenic_tcu_init(struct device_node *np) {
+	int ret;
+
+	printk("ingenic_tcu_init\n");
+
+	ret = platform_driver_register(&ingenic_tcu_driver);
+	if (ret) {
+		pr_err("Failed to register Ingenic TCU driver: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
 }
 
-CLK_OF_DECLARE_DRIVER(jz4740_cgu, "ingenic,jz4740-tcu", ingenic_tcu_init);
-CLK_OF_DECLARE_DRIVER(jz4725b_cgu, "ingenic,jz4725b-tcu", ingenic_tcu_init);
-CLK_OF_DECLARE_DRIVER(jz4760_cgu, "ingenic,jz4760-tcu", ingenic_tcu_init);
-CLK_OF_DECLARE_DRIVER(jz4770_cgu, "ingenic,jz4770-tcu", ingenic_tcu_init);
-CLK_OF_DECLARE_DRIVER(x1000_cgu, "ingenic,x1000-tcu", ingenic_tcu_init);
+module_platform_driver(ingenic_tcu_driver);
